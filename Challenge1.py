6adecde322c52a6ef21c31a132d89ef8568d506f
BenjaminDowns <benjamin.downs@gmail.com>
2015-06-12 22:15:06 +0200

    adding challenges 1 and 218

---
+++ Challenge1.py
@@ -0,0 +1,8 @@
+__author__ = 'benjamindowns'
+
+print "Hello."
+name = raw_input("Please enter your name: ")
+age = raw_input("Please enter your age (years): ")
+username = raw_input("Please enter your reddit username: ")
+
+print "your name is {0}, you are {1} years old, and your username is {2}." .format(name, age, username)
---
+++ Challenge218.py
@@ -0,0 +1,15 @@
+__author__ = 'Oops_TryAgain'
+
+number = 197
+original_number = number
+steps = 0
+
+def make_palindrome(number):
+    global steps
+    if str(number) == ((str(number))[::-1]):
+        print "{0} gets palindromic after {1} steps: {2}".format(original_number, steps, number)
+    else:
+        steps += 1
+        make_palindrome(number + (int(str(number)[::-1])))
+
+make_palindrome(number)
